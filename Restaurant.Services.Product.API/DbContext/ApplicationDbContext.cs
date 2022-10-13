8d37af9a4fdd7247dae17b7c96c3c41928e1b663
Senith Dilitha <senith@excelly.io>
2022-10-13 08:42:44 +0530

    My first commit

---
+++ Restaurant.Services.Product.API/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Restaurant.Services.Product.API.DbContext
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Models.Product> Products { get; set; }
+    }
+}
